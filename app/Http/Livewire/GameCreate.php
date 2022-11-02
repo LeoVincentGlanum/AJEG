02f238231b19e391020ee2db741eae646ccae28b
LeoVincentGlanum <leo@glanum.com>
2022-11-02 12:44:10 +0100

    wp : create game , repasse avec Pierre

--- app/Http/Livewire/GameCreate.php
+++ app/Http/Livewire/GameCreate.php
@@ -7,6 +7,8 @@ use App\Models\Game;
 use Livewire\Component;
 use App\Models\GameType;
 use App\Models\GamePlayer;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 class GameCreate extends Component
 {
@@ -14,38 +16,87 @@ class GameCreate extends Component
     public $users;
     public $type = "ask";
     public $gameTypes;
-
+    public $resultat;
     public $players = [];
+    public $selectBlanc = "nul";
+    public $playersColors = [];
 
     public $partyName;
 
-    public function mount(){
-        $this->users = User::all();
+    protected $messages = [
+        'selectBlanc.not_in' => 'Attention ! Merci de selectionner un joueur blanc',
+    ];
+
+    public function mount()
+    {
+        $this->users     = User::all();
         $this->gameTypes = GameType::all();
     }
 
 
-    public function gotto(){
+    public function gotto()
+    {
         return redirect("dashboard");
     }
 
-    public function submit(){
-        $arrayPlayer = $this->users;
+    public function updatedPlayers($value)
+    {
+         foreach ($this->playersColors as $key => $color) {
+            if (!in_array($key,$this->players)){
+                unset($this->playersColors[$key]);
+            }
+        }
+    }
+
+    public function rules()
+    {
+        if (count($this->players) > 2) {
+            return [
+                'selectBlanc' => 'sometimes',
+                'playersColors' => 'required|array|size:'. count($this->players),
+                'playersColors.*' => 'required|string'
+            ];
+        }
+
+        return [
+             'selectBlanc' => 'required|not_in:nul',
+        ];
+    }
+
+    public function submit()
+    {
+        $this->validate();
 
         $newGame = new Game();
+        $newGame->label = $this->partyName;
         $newGame->status = $this->type;
         $newGame->save();
 
-        foreach ($arrayPlayer as $player){
-            $gameplay = new GamePlayer();
-            $gameplay->game_id = $newGame->id;
+        foreach ($this->players as $player) {
+
+            if (count($this->playersColors) > 0 ){
+                $color = $this->playersColors[$player];
+            }
+            if($this->selectBlanc === $player) {
+                $color = "blanc";
+            }
+            $color = "noir";
 
 
+            if($this->resultat == "nul" || $this->resultat == "path" ){
+                $result = $this->resultat;
+            }
+
+
+            $gameplayer          = new GamePlayer();
+            $gameplayer->game_id = $newGame->id;
+            $gameplayer->user_id = $player->id;
+            $gameplayer->color   = $color;
+            $gameplayer->result  = "";
         }
-
     }
 
-       public function render()
+    public function render()
     {
         return view('livewire.game-create');
     }
--- resources/views/livewire/game-create.blade.php
+++ resources/views/livewire/game-create.blade.php
@@ -41,6 +41,44 @@
                     @endforeach
 
                     @if(count($players) > 1)
+                         @if(count($players) == 2)
+                             <h3>Couleurs du joueur blanc </h3>
+                         @if($selectBlanc === "nul")
+                              @if($errors->has('selectBlanc'))
+                                    <div class="alert alert-danger d-flex align-items-center" role="alert">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-diamond-fill" viewBox="0 0 16 16">
+                                            <path d="M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                                        </svg>
+                                        <div class="ml-2">
+                                            <span>{{ $errors->first('selectBlanc') }}</span>
+                                        </div>
+                                    </div>
+                              @endif
+                            @endif
+                            <select wire:model="selectBlanc">
+                                 <option value="nul">Selectionner le joueur blanc ...</option>
+                                @foreach($players as $player)
+                                    <option value="{{$users->find($player)->id}}">{{$users->find($player)->name}} Blanc </option>
+                                @endforeach
+                            </select>
+                         @else
+                            @foreach($players as $player)
+                                 <div class="mt-5">
+                                    <label for="colorPlayer{{$player}}" class="form-label">Couleur de {{$users->find($player)->name}}</label>
+                                    <input wire:model="playersColors.{{$player}}" class="form-control" type="text" id="colorPlayer{{$player}}">
+                                 </div>
+                            @endforeach
+                            @error('playersColors')
+                                <div class="mt-6 alert alert-danger d-flex align-items-center" role="alert">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-diamond-fill" viewBox="0 0 16 16">
+                                        <path d="M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                                    </svg>
+                                    <div class="ml-2">
+                                        <span>{{ $message }}</span>
+                                    </div>
+                                </div>
+                            @enderror
+                         @endif
 
                         <h3 class="my-3"> Status du match </h3>
 
@@ -58,33 +96,34 @@
 
                             <div class="mt-5">
                                 <label for="exampleInputEmail1" class="form-label">Resultat de la partie</label>
-                                <select class="form-control">
+                                <select class="form-control" wire:model="resultat">
+                                    <option value="none"></option>
                                     @foreach($players as $player)
-                                        <option>{{\App\Models\User::find($player)->name}} Gagnant</option>
+                                        <option value="{{$player}}">{{$users->find($player)->name}} Gagnant</option>
                                     @endforeach
-                                    <option>Path</option>
-                                    <option>Nul</option>
+                                    <option value="path">Path</option>
+                                    <option value="nul">Nul</option>
                                 </select>
                             </div>
                         @endif
-
-                        <div style="
-                            display: flex;
-                            align-items: flex-end;
-                            justify-content: flex-end;
-                            flex-direction: row-reverse;
-                        ">
-                            <button type="submit" class="btn btn-success mt-3" style="margin-left: 1vw;">
-                                Creer la partie
-                            </button>
                     @else
                             <div class="alert alert-danger mt-3" role="alert">
                                 <strong>Attention !</strong> Vous devez renseingner aux moins deux joueurs avant de passer à la suite
                             </div>
                     @endif
+
+                    <div class="mt-6 flex items-center">
+                        <button type="button" class="btn btn-secondary" wire:click="gotto">Retour</button>
+                         @if(count($players) > 1)
+                            <button type="submit" class="ml-4 btn btn-success">
+                                Creer la partie
+                            </button>
+                        @endif
+                    </div>
+
                 </form>
 
-                <button class="btn btn-secondary" wire:click="gotto" onclick="return false;">Retour</button>
+
             </div>
         </div>
     </div>
