1fdaa2eacacf6065989116478f28c6f714ef63e3
adrien-glanum <adrien@glanum.com>
2022-11-07 11:49:17 +0100

    ajout redirection sur partie en cours

--- resources/views/livewire/game/game-show.blade.php
+++ resources/views/livewire/game/game-show.blade.php
@@ -32,7 +32,7 @@
                                alt="">
                       </div>
                       <div class="min-w-0 flex-1">
-                          <a href="#" class="focus:outline-none">
+                          <a href="../../profile/{{$player->user->id}}" class="focus:outline-none">
                               <span class="absolute inset-0" aria-hidden="true"></span>
                               <p class="text-sm font-medium text-gray-900">{{$player->user->name}}</p>
                               <p class="truncate text-sm text-gray-500">Joue les {{$player->color}}</p>
