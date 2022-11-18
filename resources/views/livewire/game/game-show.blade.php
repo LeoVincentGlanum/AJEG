b364dbce0c2c612b67948e7cb055d7946bcacc98
adrien-glanum <adrien@glanum.com>
2022-11-18 10:07:11 +0100

    ajout image

--- resources/views/livewire/game/game-show.blade.php
+++ resources/views/livewire/game/game-show.blade.php
@@ -26,7 +26,7 @@
                   <div class="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400">
                       <div class="flex-shrink-0">
                           <img class="h-10 w-10 rounded-full"
-                               src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                               src="{{ asset('storage/photos/'.$player->user->photo) }}"
                                alt="">
                       </div>
                       <div class="min-w-0 flex-1">
@@ -54,7 +54,7 @@
             </button>
             </li>
 
-{{-- 
+{{--
 
     @if($game->status !== "Ended")
 
