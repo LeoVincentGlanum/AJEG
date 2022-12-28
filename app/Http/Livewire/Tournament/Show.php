900969e79f82c6edf1691c7aafa5bfc7ce09e2ee
Zac <zaccharie@glanum.com>
2022-12-28 11:37:09 +0100

    Feat: (WIP) ajout d'une page show pour les tournois

--- app/Http/Livewire/Tournament/Show.php
+++ app/Http/Livewire/Tournament/Show.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Livewire\Tournament;
 
+use App\Models\Tournament;
 use Livewire\Component;
 
 class Show extends Component
 {
+    public Tournament $tournament;
+
+    public function mount(Tournament $tournament)
+    {
+        $this->tournament = $tournament;
+    }
+
     public function render()
     {
         return view('livewire.tournament.show');
