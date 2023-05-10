de3900a6e8ac1c87e57aad94527c22eb41885278
adrien-glanum <adrien@glanum.com>
2023-05-10 09:00:56 +0200

    fix(css): center des éléments

--- resources/views/livewire/darts/onlineGame/board.blade.php
+++ resources/views/livewire/darts/onlineGame/board.blade.php
@@ -1,4 +1,13 @@
 <body>
+<style>
+    #dartboard {
+        width: 800px;
+        height: 800px;
+        margin: auto;
+        margin-top: 50px; /* ajustez la valeur de la marge en haut selon vos besoins */
+    }
+</style>
+
 <div id="dartboard" class="self-center" style="width: 800px; height: 800px;"></div>
 <script src="https://unpkg.com/dartboard/dist/dartboard.js"></script>
 </body>
---
+++ resources/views/livewire/darts/onlineGame/board2.blade.php
@@ -0,0 +1,16 @@
+<body>
+
+<div id="dartboard" class="self-center"></div>
+
+<script src="https://unpkg.com/dartboard/dist/dartboard.js"></script>
+{{--<script>--}}
+{{--    var dartboard = new Dartboard('#dartboard');--}}
+{{--    dartboard.render();--}}
+
+{{--    document.querySelector('#dartboard').addEventListener('livewire:load', function(dartScore) {--}}
+{{--        console.log(dartScore.detail.score)--}}
+{{--        Livewire.emit('updateScore', dartScore.detail.score)--}}
+{{--    });--}}
+{{--</script>--}}
+
+</body>
